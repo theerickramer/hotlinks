4e661b67bbcd1c737d533a019e786cf64de3f42a
Eric Kramer <eric.kramer@showtime.net>
2017-01-23 11:56:30 -0500

    added server delete method

--- server.js
+++ server.js
@@ -26,6 +26,17 @@ app.get('/', function(req, res) {
 	res.render('index.html');
 })
 
+app.get('/hotlinks', function(req, res) {
+	var hotlinks = Hotlink.find({}, function(err, hotlinks) {
+		if (err) {
+			console.error(err);
+			res.json('Error: Hotlinks could not be loaded');
+		} else {
+			res.json(hotlinks);
+		}
+	})
+});
+
 app.post('/hotlinks', function(req, res) {
 	var hotlink = new Hotlink({ hotlink: req.body.hotlink });
 	hotlink.save()
@@ -38,13 +49,13 @@ app.post('/hotlinks', function(req, res) {
 		})
 });
 
-app.get('/hotlinks', function(req, res) {
-	var hotlinks = Hotlink.find({}, function(err, hotlinks) {
-		if (err) {
-			console.error(err);
-			res.json('Error: Hotlinks could not be loaded');
-		} else {
-			res.json(hotlinks);
-		}
-	})
+app.delete('/hotlinks', function(req, res) {
+	Hotlink.findByIdAndRemove(req.body.id)
+		.then(function(query){
+			console.log(query + ' deleted')
+			res.json(query + ' deleted');
+		})
+		.catch(function(error) { 
+			console.error(error)
+		})
 });
