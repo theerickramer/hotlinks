adf85fb6028300af28d01955fed6e4e230f526af
Eric Kramer <theerickramer@gmail.com>
2017-12-15 11:42:07 -0500

    style loaders prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,29 +3,45 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: path.resolve(__dirname, 'src/index.js'),
-	output: {
-		path: path.resolve(__dirname, 'dist/'),
-		filename: 'bundle.js',
-		publicPath: '/'
-	},
-	module: {
-		loaders: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader',
-		}]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: path.resolve(__dirname, 'views/index.html'),
-			template: 'template.html',
-		}),
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production')
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin()
-	]
-}
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'views/index.html'),
+      template: 'template.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+};
